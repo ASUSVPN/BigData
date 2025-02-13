5505fb91bc4e9be777b3221626e11ad05e4b676e
KatherynneCPlessmann <katherynne.plessmann@uabc.edu.mx>
2025-02-12 19:49:33 -0800

    Kat inventando clase 1

---
+++ SCALA_BASICS_KAT/Practica_01_Kat.scala
@@ -0,0 +1,9 @@
+// Assessment 1 / Practica 1
+// 1. Implementa un programa en scala que calcule el radio de un circulo
+// 2. Implementa un programa en scala que me diga si un numero es primo
+// 3. Dada la variable bird = "tweet", utiliza interpolacion de strings 
+// para imprimir "Estoy escribiendo un tweet"
+// 4. Dada la variable  mensaje = "Hola Luke yo soy tu padre! utilizas 
+//slice para extraer la secuencia "Luke"
+// Cual es la diferencia entre value una variable var en scala?
+// 6. Dada la tupla (2, 4, 5, 1, 2, 3.1416, 3, 7) imprime "3.1416"
---
+++ SCALA_BASICS_KAT/Session_02.scala
@@ -0,0 +1,10 @@
+vsl st = "item1 item2 item3"
+st matches "item1 item2 item3"
+
+//Tuplas
+(1 , 2.3, "Hello")
+
+val myTup = (1, 2.2 , "hello", 23.2, true)
+myTup._3
+val myTup = (3, 1, (3, 6))
+myTup._3._2
