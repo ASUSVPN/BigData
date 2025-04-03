bf5880f9af1a15fcfd4bdab3cb07861370290dd3
KatherynneCPlessmann <katherynne.plessmann@uabc.edu.mx>
2025-04-02 18:56:41 -0700

    Preaparando ambiente para random forest

--- SCALA_BASICS_KAT/test.scala
+++ SCALA_BASICS_KAT/test.scala
@@ -1,100 +1,84 @@
-// Practica 2
+////////////////////////////////////////////
+//// LINEAR REGRESSION EXERCISE ///////////
+/// Coplete las tareas comentadas ///
+/////////////////////////////////////////
 
-// 1. Crea una lista llamada "lista" con los elementos "rojo", "blanco", "negro"
-val lista1 = List("rojo", "blanco", "negro")
-// 2. Añadir 5 elementos mas a "lista" "verde" ,"amarillo", "azul", "naranja", "perla"
-val lista2 = List("rojo", "blanco", "negro","verde" ,"amarillo", "azul", "naranja", "perla")
-// 3. Traer los elementos de "lista" "verde", "amarillo", "azul"
-lista2(3)
-lista2(4)
-lista2(5)
-// 4. Crea un arreglo de numero en rango del 1-1000 en pasos de 5 en 5
-val arreglo = Array.range(1, 1001, 5)
+// Import LinearRegression
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.regression.LinearRegression
 
-// 5. Cuales son los elementos unicos de la lista Lista(1,3,3,4,6,7,3,7) utilice conversion a conjuntos
-// 6. Crea una mapa mutable llamado nombres que contenga los siguiente
-//     "Jose", 20, "Luis", 24, "Ana", 23, "Susana", "27"
-// 6 a . Imprime todas la llaves del mapa
-// 7 b . Agrega el siguiente valor al mapa("Miguel", 23)
+// Opcional: Utilice el siguiente codigo para configurar errores
+import org.apache.log4j._
+Logger.getLogger("org").setLevel(Level.ERROR)
 
-// Practica 3
 
-// def isEven(num:Int): Boolean = {
-//     return num%2 == 0
-// }
-// def isEven(num:Int): num%2 == 0
-// println(isEven(6))
-// println(isEven(3))
-// Practice 3, analyse the following code with your own words
+// Inicie una simple Sesion Spark
+val spark = SparkSession.builder().getOrCreate()
 
-//Clasifica cada número como par o impar
-def listEvens(list:List[Int]): String ={ 
-    for(n <- list){ // Itera sobre cada elemento 'n' de la lista
-        if(n%2==0){ // Evalua si 'n' es divisible por 2 
-            println(s"$n is even") // Imprime que es par
-        }else{ //si no es divisible por 2
-            println(s"$n is odd") // Imprime que es impar
-        }
-    }
-    return "Done" // Cuando de termina dice Done
+// Utilice Spark para el archivo csv Clean-Ecommerce .
+val data  = spark.read.option("header","true").option("inferSchema", "true").format("csv").load("Clean-Ecommerce.csv")
+
+// Imprima el schema en el DataFrame.
+data.printSchema
+
+// Imprima un renglon de ejemplo del DataFrane.
+data.head(1)
+
+//////////////////////////////////////////////////////
+//// Configure el DataFrame para Machine Learning ////
+//////////////////////////////////////////////////////
+
+// Transforme el data frame para que tome la forma de
+// ("label","features")
+val colnames = data.columns
+val firstrow = data.head(1)(0)
+println("\n")
+println("Example data row")
+for(ind <- Range(1, colnames.length)){
+    println(colnames(ind))
+    println(firstrow(ind))
+    println("\n")
 }
 
-val l = List(1,2,3,4,5,6,7,8)
-val l2 = List(4,3,22,55,7,8)
-listEvens(l)
-listEvens(l2)
+// Importe VectorAssembler y Vectors
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.linalg.Vectors
 
-//3 7 afortunado
+// Renombre la columna Yearly Amount Spent como "label"
+// Tambien de los datos tome solo la columa numerica 
+// Deje todo esto como un nuevo DataFrame que se llame df
+data.columns
+val df = data.select(data("Yearly Amount Spent").as("label"), $"Avg Session Length", $"Time on App", $"Time on Website", $"Length of Membership")
 
-def afortunado(list:List[Int]): Int={
-    var res=0
-    for(n <- list){
-        if(n==7){
-            res = res + 14
-        }else{
-            res = res + n
-        }
-    }
-    return res
-}
+// Configure las columnas de entrada de donde se supone que leemos los valores.
+// Llamar a esto nuevo assambler.
+// Que el objeto assembler convierta los valores de entrada a un vector
+val assembler = new VectorAssembler().setInputCols(Array("Avg Session Length", "Time on App", "Time on Website", "Length of Membership")).setOutputCol("features")
 
-val af= List(1,7,7)
-println(afortunado(af))
+// Utilice el assembler para transformar nuestro DataFrame a dos columnas: label and features
+// Utilice el objeto VectorAssembler para convertir las columnas de entrada del df
+// a una sola columna de salida de un arreglo llamado "features"
+val output = assembler.transform(df).select($"label", $"features")
 
-def balance(list:List[Int]): Boolean={
-    var primera = 0
-    var segunda = 0
+// Crear un objeto para modelo de regresion linea.
+val lr = new LinearRegression()
 
-    segunda = list.sum
+// Ajuste el modelo para los datos y llame a este modelo lrModelo
+val lrModel = lr.fit(output)
 
-    for(i <- Range(0,list.length)){
-        primera = primera + list(i)
-        segunda = segunda - list(i)
+// Imprima the  coefficients y intercept para la regresion lineal
+println(s"Coeficientes: ${lrModel.coefficients}")
+println(s"Intercepto: ${lrModel.intercept}")
 
-        if(primera == segunda){
-            return true
-        }
-    }
-    return false 
-}
 
-val bl = List(3,2,1)
-val bl2 = List(2,3,3,2)
-val bl3 = List(10,30,90)
+// Utilize metodo .summary de nuestro  modelo para crear un objeto llamado trainingSummary
+val trainingSummary = lrModel.summary
 
-balance(bl)
-balance(bl2)
-balance(bl3)
+// Resuma el modelo sobre el conjunto de entrenamiento imprima la salida de algunas metricas!
+// Muestre los valores de residuals, el RMSE, el MSE, y tambien el R^2 .
 
-//Detecta palabras que se pueden leer igual de izquierda a derecha y viceversa
-def palindromo(palabra:String):Boolean ={ //La funcion recibe un String y devuelve un Boolean
-    return (palabra == palabra.reverse) // Se compara la palabra original con su versión invertida
-}
+trainingSummary.residuals.show()
 
-val palabra = "OSO" //igual en ambos sentidos
-val palabra2 = "ANNA" //igual en ambos sentidos
-val palabra3 = "JUAN" // no es igual en ambos sentidos
-
-println(palindromo(palabra))
-println(palindromo(palabra2))
-println(palindromo(palabra3))
+println(s"RMSE (Raíz del Error Cuadrático Medio): ${trainingSummary.rootMeanSquaredError}")
+println(s"MSE (Error Cuadrático Medio): ${trainingSummary.meanSquaredError}")
+println(s"R2 (Coeficiente de determinación): ${trainingSummary.r2}")
